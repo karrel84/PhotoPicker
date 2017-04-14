a2e9408f37fbd14c5160655134d68f6e645ffc96
karrel <karrel84@naver.com>
2017-04-14 13:52:16 +0900

    Initial commit

---
+++ app/src/main/java/com/kerrel/photopicker/MainActivity.java
@@ -0,0 +1,59 @@
+package com.kerrel.photopicker;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int RESULT_LOAD_IMG = 9000;
+    private ImageView mPicture;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        mPicture = (ImageView) findViewById(R.id.picture);
+        findViewById(R.id.get_image).setOnClickListener(onGetImageListener);
+    }
+
+    private View.OnClickListener onGetImageListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+            photoPickerIntent.setType("image/*");
+            startActivityForResult(photoPickerIntent, RESULT_LOAD_IMG);
+        }
+    };
+
+    @Override
+    protected void onActivityResult(int reqCode, int resultCode, Intent data) {
+        super.onActivityResult(reqCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            try {
+                final Uri imageUri = data.getData();
+                final InputStream imageStream = getContentResolver().openInputStream(imageUri);
+                final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+                mPicture.setImageBitmap(selectedImage);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                Toast.makeText(MainActivity.this, "Something went wrong", Toast.LENGTH_LONG).show();
+            }
+
+        } else {
+            Toast.makeText(MainActivity.this, "You haven't picked Image", Toast.LENGTH_LONG).show();
+        }
+    }
+}
